6e9b719b53791c04dbcb83f52c728025d0c9076e
Kip Cole <kipcole9@gmail.com>
2021-10-19 21:41:40 +0800

    Fix tests to work with ex_cldr 2.24 (CLDR 40)

--- lib/cldr/backend.ex
+++ lib/cldr/backend.ex
@@ -206,7 +206,7 @@ defmodule Cldr.Territory.Backend do
             {:error, {Cldr.UnknownLocaleError, "The locale :zzz is not known."}}
 
             iex> #{inspect __MODULE__}.from_territory_code(:GB, [locale: "zzz"])
-            {:error, {Cldr.UnknownLocaleError, "The locale \\"zzz\\" is not known."}}
+            {:error, {Cldr.InvalidLanguageError, "The language \\"zzz\\" is invalid"}}
 
         """
         @spec from_territory_code(Cldr.Territory.atom_binary_tag(), Cldr.Territory.options()) :: {:ok, binary()} | {:error, Cldr.Territory.error()}
@@ -251,7 +251,7 @@ defmodule Cldr.Territory.Backend do
             {:ok, "Kumbria"}
 
             iex> #{inspect __MODULE__}.from_subdivision_code("gbcma", locale: "bs")
-            {:error, {Cldr.UnknownSubdivisionError, "The locale \\"bs\\" has no translation for \\"gbcma\\"."}}
+            {:error, {Cldr.UnknownSubdivisionError, "The locale \\"bs\\" has no translation for :gbcma."}}
 
             iex> #{inspect __MODULE__}.from_subdivision_code("invalid", locale: "en")
             {:error, {Cldr.UnknownTerritoryError, "The territory \\"invalid\\" is unknown"}}
@@ -260,7 +260,7 @@ defmodule Cldr.Territory.Backend do
             {:error, {Cldr.UnknownLocaleError, "The locale :zzz is not known."}}
 
             iex> #{inspect __MODULE__}.from_subdivision_code("gbcma", [locale: "zzz"])
-            {:error, {Cldr.UnknownLocaleError, "The locale \\"zzz\\" is not known."}}
+            {:error, {Cldr.InvalidLanguageError, "The language \\"zzz\\" is invalid"}}
 
         """
         @spec from_subdivision_code(binary(), [locale: Cldr.Territory.binary_tag()]) :: {:ok, binary()} | {:error, Cldr.Territory.error()}
@@ -310,7 +310,6 @@ defmodule Cldr.Territory.Backend do
         def from_territory_code!(territory_code, options) do
           case from_territory_code(territory_code, options) do
             {:error, {exception, msg}} -> raise exception, msg
-
             {:ok, result}              -> result
           end
         end
@@ -440,7 +439,7 @@ defmodule Cldr.Territory.Backend do
             {:ok, "Kumbria"}
 
             iex> #{inspect __MODULE__}.translate_subdivision("Cumbria", "en", "bs")
-            {:error, {Cldr.UnknownSubdivisionError, "The locale \\"bs\\" has no translation for \\"gbcma\\"."}}
+            {:error, {Cldr.UnknownSubdivisionError, "The locale \\"bs\\" has no translation for :gbcma."}}
 
             iex> #{inspect __MODULE__}.translate_subdivision("Cumbria", :zzz)
             {:error, {Cldr.UnknownLocaleError, "The locale :zzz is not known."}}
@@ -925,11 +924,9 @@ defmodule Cldr.Territory.Backend do
           |> Cldr.validate_locale(unquote(backend))
           |> case do
               {:error, error}                                    -> {:error, error}
-
               {:ok, %LanguageTag{cldr_locale_name: locale_name}} -> {:ok, code, locale_name}
             end
         end
-
 
         @doc """
         Unicode flag for the given territory code.
