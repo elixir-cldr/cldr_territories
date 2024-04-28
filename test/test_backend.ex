edb05032bb5bd52308405b816b30be1b0cc8e486
Benjamin Schultzer <benjamin@schultzer.com>
2024-04-28 16:47:43 -0400

    Test against all backends
    # Conflicts:
    #	mix.exs

--- test/test_backend.ex
+++ test/test_backend.ex
@@ -6,3 +6,12 @@ defmodule TestBackend.Cldr do
     locales: ["fr", "zh", "en", "bs", "pl", "ru", "th", "he", "pt", "en-001", "nl"],
     providers: [Cldr.Territory]
 end
+
+defmodule TestAllBackends.Cldr do
+  require Cldr.Territory.Backend
+
+  use Cldr,
+    default_locale: "en-001",
+    locales: ["en-001"],
+    providers: [Cldr.Calendar, Cldr.DateTime, Cldr.Language, Cldr.List, Cldr.Message, Cldr.Number, Cldr.Territory, Cldr.Unit]
+end
